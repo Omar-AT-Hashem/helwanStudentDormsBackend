e148791f7e3a24698899db1c135f1f0d093e7b1c
Omar-AT-Hashem <omar.at.hashem@gmail.com>
2023-10-05 21:11:18 +0300

    added CRUD operations for instructions

--- routes/general/handlers/instructions.js
+++ routes/general/handlers/instructions.js
@@ -1,8 +1,6 @@
 import { Router } from "express";
 import conn from "../../../config/db.js";
-
-
-
+import authenticateToken from "../../../middleware/authenticateToken.js";
 
 const instructions = Router();
 
@@ -15,9 +13,69 @@ async function index(req, res) {
   }
 }
 
+async function create(req, res) {
+  try {
+    const { instruction } = req.body;
 
+    if (!instruction) {
+      return res
+        .status(400)
+        .json({ message: "Please provide all the required fields" });
+    }
 
-instructions.get("/",  index);
+    const created = await conn.awaitQuery(
+      "INSERT INTO instructions (instruction) VALUES (?)",
+      [instruction]
+    );
 
+    res
+      .status(201)
+      .json({ message: "instruction created", id: created.insertId });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+}
+
+async function update(req, res) {
+  try {
+    const { id, instruction } = req.body;
+
+    if (!instruction || !id) {
+      return res
+        .status(400)
+        .json({ message: "Please provide all the required fields" });
+    }
+
+     await conn.awaitQuery(
+      "UPDATE instructions SET instruction = ? WHERE id = ?",
+      [instruction, id]
+    );
+
+    res.status(201).json({ message: "instruction Updated" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+}
+
+async function deleteById(req, res) {
+  const instructionId = req.params.id;
+  try {
+    await conn.awaitQuery("DELETE FROM instructions WHERE id = ?", [
+      instructionId,
+    ]);
+    return res.status(200).json({
+      message: `Instruction with Id = ${instructionId} was deleted successfully`,
+    });
+  } catch (err) {
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+}
+
+instructions.get("/", index);
+instructions.post("/", create);
+instructions.put("/", update);
+instructions.delete("/:id", deleteById);
 
 export default instructions;
